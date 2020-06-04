bc12a130c352ba7e3a3bd5642968ae50ef6fb815
Jacob Andronicos <jandronicos2@gmail.com>
2020-06-04 11:45:08 +1000

    Changed AccountController to be less cluttered, edit the session as well as the DB so the updated details are displayed correctly.

--- src/main/java/group3/seng3150/BookingsController.java
+++ src/main/java/group3/seng3150/BookingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import javax.persistence.EntityManager;
+import javax.servlet.http.HttpSession;
 import java.sql.Date;
 import java.util.List;
 
@@ -21,30 +22,40 @@ public class BookingsController {
 
     //get method show bookings
     @GetMapping("/flightBooking")
-    public ModelAndView showBooking() {
+    public ModelAndView showBooking(HttpSession session) {
         ModelAndView view = new ModelAndView("flightBooking");
-        /*
         em.getTransaction().begin();
 
         Booking newBooking = new Booking();
-
-
-
+        newBooking.setAirlineCode("");
+        newBooking.setAirlineCode2("");
+        newBooking.setAirlineCode3("");
+        newBooking.setAirlineCode4("");
+        newBooking.setAirlineCode4("");
+        //newBooking.setBookingDate(testDate);
+        newBooking.setClassCode("");
+        newBooking.setClassCode2("");
+        newBooking.setClassCode3("");
+        newBooking.setClassCode4("");
+        //newBooking.setDepartureTime(testDate);
+        //newBooking.setDepartureTime2(testDate);
+        //newBooking.setDepartureTime3(testDate);
+        //newBooking.setDepartureTime4(testDate);
         em.persist(newBooking);
         em.getTransaction().commit();
-        */
+
         return view;
     }
 
     //get method manage bookings
     @GetMapping("/manageBooking")
-    public ModelAndView manageBooking() {
+    public ModelAndView manageBooking(HttpSession session) {
         ModelAndView view = new ModelAndView("manageBooking");
         return view;
     }
 
     @GetMapping("/bookingtemp")
-    public ModelAndView displayBooking() {
+    public ModelAndView displayBooking(HttpSession session) {
         ModelAndView view = new ModelAndView("flightBooking");
         return view;
     }
