17d265c1013c1ccd4ddc600167736c706b8d2ddb
Jacob Andronicos <jandronicos2@gmail.com>
2020-06-05 14:20:05 +1000

    Flightbooking has been split into return and oneway.

--- src/main/java/group3/seng3150/BookingsController.java
+++ src/main/java/group3/seng3150/BookingsController.java
@@ -87,10 +87,10 @@ public class BookingsController {
         return view;
     }
 
-    @PostMapping("/bookingtemp")
+    @PostMapping("/flightBookingReturn")
     public ModelAndView displayBooking(HttpSession session,
-                                       @RequestParam(name="onewayFlightPlan") int positionDeparture,
-                                       @RequestParam(name="returnFlightPlan") int positionReturn,
+                                       @RequestParam(name="flightPlan") String positionDepartureS,
+                                       @RequestParam(name="return") String positionReturnS,
                                        @RequestParam(name="trip") String trip,
                                        @RequestParam(name="onewayAdultsBooking") int onewayAdultsBooking,
                                        @RequestParam(name="onewayChildrenBooking") int onewayChildrenBooking,
@@ -99,7 +99,8 @@ public class BookingsController {
                                        @RequestParam(name="returnChildrenBooking") int returnChildrenBooking,
                                        @RequestParam(name="returnClassBooking") String returnClassBooking
     ){
-
+        int positionDeparture = Integer.parseInt(positionDepartureS);
+        int positionReturn = Integer.parseInt(positionReturnS);
         //create a List of new bookings with the flight details selected, and add it to the session for the actual booking page to receive payment
         //and persist it
         ModelAndView view = new ModelAndView("flightBooking");
@@ -459,6 +460,191 @@ public class BookingsController {
         //*****************SUBJECT TO CHANGE**********************************
         view.addObject("returnFlight", flightPlan.getFlights().get(0));
         //*****************SUBJECT TO CHANGE**********************************
+        return view;
+    }
+
+    @PostMapping("/flightBookingOneway")
+    public ModelAndView displayBookingOneway(HttpSession session,
+                                       @RequestParam(name="flightPlan") String positionDepartureS,
+                                       @RequestParam(name="onewayAdultsBooking") int onewayAdultsBooking,
+                                       @RequestParam(name="onewayChildrenBooking") int onewayChildrenBooking,
+                                       @RequestParam(name="onewayClassBooking") String onewayClassBooking
+    ){
+        int positionDeparture = Integer.parseInt(positionDepartureS);
+
+        //create a List of new bookings with the flight details selected, and add it to the session for the actual booking page to receive payment
+        //and persist it
+        ModelAndView view = new ModelAndView("flightBooking");
+        List<Booking> bookingsDeparture = new List<Booking>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Booking> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Booking booking) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Booking> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends Booking> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+
+            @Override
+            public Booking get(int index) {
+                return null;
+            }
+
+            @Override
+            public Booking set(int index, Booking element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, Booking element) {
+
+            }
+
+            @Override
+            public Booking remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Booking> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<Booking> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<Booking> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+
+        FlightHolder searchDeparture = (FlightHolder) session.getAttribute("departureFlights");
+        searchDeparture.setFlightPlanPositions();
+        FlightPlan flightPlan = searchDeparture.getFlightPlans().get(positionDeparture);
+        flightPlan.getDepartureDate();
+
+        for (int i = 0; i < onewayAdultsBooking + onewayChildrenBooking; i++) {
+            Booking newBooking = new Booking();
+            newBooking.setGroupSize(onewayAdultsBooking + onewayChildrenBooking);
+            //Works based on a flightPlan having no more than 4 flights, as per assumptions for bookings
+            for (int j = 0; j < flightPlan.getFlights().size(); j++) {
+                if (j == 0) {
+                    newBooking.setAirlineCode(flightPlan.getFlights().get(j).getAirlineCode());
+                    newBooking.setDepartureTime(flightPlan.getFlights().get(j).getDepartureDate());
+                    newBooking.setBookingDate(flightPlan.getFlights().get(j).getArrivalDate());
+                    newBooking.setFlightNumber(flightPlan.getFlights().get(j).getFlightNumber());
+                    newBooking.setTicketCode(generateTicketNumber());
+                    newBooking.setClassCode(onewayClassBooking);
+                } else if (j == 1) {
+                    newBooking.setAirlineCode2(flightPlan.getFlights().get(j).getAirlineCode());
+                    newBooking.setDepartureTime2(flightPlan.getFlights().get(j).getDepartureDate());
+                    newBooking.setFlightNumber2(flightPlan.getFlights().get(j).getFlightNumber());
+                    newBooking.setTicketCode2(generateTicketNumber());
+                    newBooking.setClassCode2(onewayClassBooking);
+                } else if (j == 2) {
+                    newBooking.setAirlineCode3(flightPlan.getFlights().get(j).getAirlineCode());
+                    newBooking.setDepartureTime3(flightPlan.getFlights().get(j).getDepartureDate());
+                    newBooking.setFlightNumber3(flightPlan.getFlights().get(j).getFlightNumber());
+                    newBooking.setTicketCode3(generateTicketNumber());
+                    newBooking.setClassCode3(onewayClassBooking);
+                } else if (j == 3) {
+                    newBooking.setAirlineCode4(flightPlan.getFlights().get(j).getAirlineCode());
+                    newBooking.setDepartureTime4(flightPlan.getFlights().get(j).getDepartureDate());
+                    newBooking.setFlightNumber4(flightPlan.getFlights().get(j).getFlightNumber());
+                    newBooking.setTicketCode4(generateTicketNumber());
+                    newBooking.setClassCode4(onewayClassBooking);
+                }
+            }
+            bookingsDeparture.add(newBooking);
+        }
+
+        //departureBookings is a List<Booking> that holds the bookings created with the selected flight plan detail put in, but without the user data yet
+        session.setAttribute("departureBookings", bookingsDeparture);
+        view.addObject("departureBookings", bookingsDeparture);
         return view;
     }
 
