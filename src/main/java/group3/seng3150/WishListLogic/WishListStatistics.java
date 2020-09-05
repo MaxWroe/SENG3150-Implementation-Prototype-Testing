0534f374e219da46d2c00837b7268475a08d4823
Angus <asimmons@highwaygroup.net>
2020-09-05 10:45:05 +1000

    added wishlist entity and updated Q's

---
+++ src/main/java/group3/seng3150/WishListLogic/WishListStatistics.java
@@ -0,0 +1,19 @@
+package group3.seng3150.WishListLogic;
+
+import javax.persistence.EntityManager;
+
+public class WishListStatistics {
+
+
+    private EntityManager em;
+
+    public WishListStatistics(EntityManager em) {
+        this.em = em;
+    }
+
+
+    public void getStats(){
+        //returns some form of "hot location" data
+
+    }
+}
