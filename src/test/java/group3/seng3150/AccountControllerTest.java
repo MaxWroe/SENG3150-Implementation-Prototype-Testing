88c1f2c6adf32fa966547f84dd96307337f95845
MaxWroe <max.wroe@outlook.com>
2020-10-27 13:42:11 +1100

    Revert "AccountControllerTest, have a test case running"
    This reverts commit af14f76d

--- src/test/java/group3/seng3150/AccountControllerTest.java
+++ src/test/java/group3/seng3150/AccountControllerTest.java
@@ -1,36 +1,24 @@
 package group3.seng3150;
 
-import group3.seng3150.dao.IUserAccountDAO;
-import group3.seng3150.entities.UserAccount;
+import group3.seng3150.dao.UserAccountDAO;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
-import org.mockito.Matchers;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -39,7 +27,7 @@ public class AccountControllerTest {
 
     @Autowired AccountController cont;
     @Autowired WebApplicationContext ctx;
-    @Autowired IUserAccountDAO dao;
+    @Autowired UserAccountDAO dao;
     MockMvc mockMvc;
 
     @Before
@@ -49,21 +37,7 @@ public class AccountControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "max.wroe@outlook.com")
-    //This test checks that get account details returns the correct details of a given user to the view, first test case
-    public void testDisplayAccountDetails() throws Exception {
+    public void testGetAccountDetails() {
 
-        UserAccount testAccount = new UserAccount("Max","Wroe","max.wroe@outlook.com",0,"Aus",0,"98 Everton St Hamilton","Dijana Jokovic","Dijana Jokovic",431176489);
-
-        Mockito.when(dao.getAccountFromEmail("max.wroe@outlook.com")).thenReturn(testAccount);
-
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/accountDetails")).andReturn();
-        ModelAndView result = mvcResult.getModelAndView();
-        Map<String, Object> model = result.getModel();
-
-        assertEquals("The first name is not the expected first name",model.get("firstName"),testAccount.getFirstName());
-        assertEquals(model.get("lastName"),testAccount.getLastName());
-        assertEquals(model.get("email"),testAccount.getEmail());
     }
-
 }
